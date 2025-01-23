d1c6e87fc1f93d16c3ea71a0ce8233ae003f6835
lucasazvd <lucasazevedooliveira@gmail.com>
2025-01-23 00:25:38 -0300

    better folders and files names (fixed)

--- ConvertPngToAscii.sh
+++ ConvertPngToAscii.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Convert all PNG files in the countries_png directory to ASCII art
+# Convert all PNG files in the pngs/ directory to ASCII arts
 # Create a new directory to store the ASCII art files
-cp -r png-countries/* ascii-countries/
+cp -r pngs/* ascii/
 # Convert all PNG files to ASCII art
-find "countries_ascii" -type f -name "*.png" -exec sh -c 'ascii-image-converter "$1" -W 32 -b > "${1%.png}.txt"' _ {} \;
+find "ascii" -type f -name "*.png" -exec sh -c 'ascii-image-converter "$1" -W 32 -b > "${1%.png}.txt"' _ {} \;
 # Remove the original PNG files
-find "countries_ascii" -type f -name "*.png" -exec rm -f {} \;
+find "ascii" -type f -name "*.png" -exec rm -f {} \;
 
